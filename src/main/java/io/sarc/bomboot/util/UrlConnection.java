fea2a1fa36aac4e42c7c8b23e1a5f079b5289b9a
jins <jins@10.160.137.227>
2022-07-18 17:35:46 +0900

    Commit.

--- src/main/java/io/sarc/bomboot/util/UrlConnection.java
+++ src/main/java/io/sarc/bomboot/util/UrlConnection.java
@@ -13,8 +13,9 @@ import org.slf4j.LoggerFactory;
 public class UrlConnection {
 	private static Logger log = LoggerFactory.getLogger(UrlConnection.class);
 
-	private static int connectTimeout = 1000;
-	private static int baseSocketTimeout = 1000;
+	private final static int connectTimeout = 1000;
+	private final static int baseSocketTimeout = 1000;
+	private final static int minSocketTimeoutMs = 5;
 	
 	public static void httpConnection(String target) {
 		log.info(target);
@@ -27,8 +28,8 @@ public class UrlConnection {
 						
 			http.setConnectTimeout(connectTimeout);
 			
-			if ( socketTimeout < 5 )
-				socketTimeout = 5;
+			if ( socketTimeout < minSocketTimeoutMs )
+				socketTimeout = minSocketTimeoutMs;
 			
             http.setReadTimeout(socketTimeout * baseSocketTimeout);
             
@@ -49,5 +50,42 @@ public class UrlConnection {
 			log.error(e.getMessage());
 			e.printStackTrace();
 		}
+	}
+	
+	public static String getHttpConnectionResult(String target) {
+		log.info(target);
+
+		int socketTimeout = RandomUtil.getRandomNumber(10);
+		
+		StringBuffer sb = new StringBuffer();
+		
+		try {
+			URL url = new URL(target);
+			HttpURLConnection http = (HttpURLConnection)url.openConnection();
+						
+			http.setConnectTimeout(connectTimeout);
+			
+			if ( socketTimeout < minSocketTimeoutMs )
+				socketTimeout = minSocketTimeoutMs;
+			
+            http.setReadTimeout(socketTimeout * baseSocketTimeout);
+            
+			BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
+			
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null) {
+				sb.append(inputLine); 
+			}
+			
+			in.close();
+		} catch (MalformedURLException e) {
+			log.error(e.getMessage());
+		} catch (IOException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return sb.toString();
 	}
 }
